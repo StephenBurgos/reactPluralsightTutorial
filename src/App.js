8ada287844c45a71045db4fd6f035538ef7dc7bd
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-22 15:01:52 -0500

    Finished Pluralsight> React> Getting Started> The Basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function Button(props) {
+  const handleClick = () => props.onClickFunction(props.increment)
+  return (
+    <button onClick={handleClick}>
+      +{props.increment}
+    </button>
+  );
+}
+
+function Display(props) {
+  return (
+    <div>
+      <div>{props.message}</div>
+    </div>
+  );
+}
+
+function PsTutorial() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+  return (
+    <React.Fragment>
+      <Button onClickFunction={incrementCounter} increment={1}/>
+      <Button onClickFunction={incrementCounter} increment={5}/>
+      <Button onClickFunction={incrementCounter} increment={10}/>
+      <Button onClickFunction={incrementCounter} increment={100}/>
+      <Display message={counter}/>
+    </React.Fragment>
+  )
+}
 
 class App extends Component {
   render() {
@@ -11,6 +42,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <PsTutorial />
           <a
             className="App-link"
             href="https://reactjs.org"
