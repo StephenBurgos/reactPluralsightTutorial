3eaedb17f63c2f791c0d4ee63ad8b8dcc94aedd2
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-22 16:43:33 -0500

    Finished Pluralsight> React> Getting Started> The GitHub Cards App> Taking Input from the User.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card {...profile} />)}
+  </div>
+);
+
+class Card extends Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div className="github-profile">
+        <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
 function Button(props) {
   const handleClick = () => props.onClickFunction(props.increment)
   return (
@@ -33,7 +60,32 @@ function PsTutorial() {
   )
 }
 
+class Form extends Component {
+  state = { userName: '' };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.userName);
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({ userName: event.target.value })}
+          placeholder="Github username"
+          required />
+        <button>Add Card</button>
+      </form>
+    );
+  }
+}
+
 class App extends Component {
+  state = {
+    profiles: testData,
+  };
+
   render() {
     return (
       <div className="App">
@@ -43,6 +95,8 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <PsTutorial />
+          <Form />
+          <CardList profiles={this.state.profiles} />
           <a
             className="App-link"
             href="https://reactjs.org"
