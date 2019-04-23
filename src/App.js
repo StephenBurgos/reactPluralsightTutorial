1afd9d94cae1198344f0e325caedb24df7ed571c
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-23 12:00:17 -0500

    Finished adding ui for the various states of the star game in the Pluralsight course.

--- src/App.js
+++ src/App.js
@@ -2,110 +2,114 @@ import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const testData = [
-  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
-  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
-  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
-];
-
-const CardList = (props) => (
-  <div>
-    {props.profiles.map(profile => <Card {...profile} />)}
-  </div>
+const StarsDisplay = props => (
+  <React.Fragment>
+    {
+      utils.range(1, props.count).map(starId =>
+        <div key={starId} className="star" />
+      )
+    }
+  </React.Fragment>
 );
 
-class Card extends Component {
-  render() {
-    const profile = this.props;
-    return (
-      <div className="github-profile">
-        <img src={profile.avatar_url} />
-        <div className="info">
-          <div className="name">{profile.name}</div>
-          <div className="company">{profile.company}</div>
-        </div>
-      </div>
-    );
-  }
-}
+const NumberDisplay = props => (
+  <button className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => {
+      console.log('Num', props.number)
+    }}>
+    {props.number}
+  </button>
+);
 
-function Button(props) {
-  const handleClick = () => props.onClickFunction(props.increment)
-  return (
-    <button onClick={handleClick}>
-      +{props.increment}
-    </button>
-  );
-}
+const StarMatch = () => {
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1,9));
+  const [candidateNums, setCandidateNums] = useState([]);
 
-function Display(props) {
-  return (
-    <div>
-      <div>{props.message}</div>
-    </div>
-  );
-}
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
 
-function PsTutorial() {
-  const [counter, setCounter] = useState(0);
-  const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
-  return (
-    <React.Fragment>
-      <Button onClickFunction={incrementCounter} increment={1}/>
-      <Button onClickFunction={incrementCounter} increment={5}/>
-      <Button onClickFunction={incrementCounter} increment={10}/>
-      <Button onClickFunction={incrementCounter} increment={100}/>
-      <Display message={counter}/>
-    </React.Fragment>
-  )
-}
-
-class Form extends Component {
-  state = { userName: '' };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state.userName);
-  }
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.userName}
-          onChange={(event) => this.setState({ userName: event.target.value })}
-          placeholder="Github username"
-          required />
-        <button>Add Card</button>
-      </form>
-    );
-  }
-}
-
-class App extends Component {
-  state = {
-    profiles: testData,
+  const numberStatus = (number) => {
+    if (!availableNums.includes(number)) {
+      return 'used';
+    }
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong' : 'candidate';
+    }
+    return 'available';
   };
 
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className="body">
+        <div className="left">
+          <StarsDisplay count={stars} />
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number =>
+            <NumberDisplay
+              key={number}
+              status={numberStatus(number)}
+              number={number}
+            />
+          )}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
+
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  // create an array of numbers between min and max (edges included)
+  range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+  // pick a random number between min and max (edges included)
+  random: (min, max) => min + Math.floor(max * Math.random()),
+
+  // Given an array of numbers and a max...
+  // Pick a random sum (< max) from the set of all available sums in arr
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length)];
+  },
+};
+
+class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <PsTutorial />
-          <Form />
-          <CardList profiles={this.state.profiles} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div>
+          <StarMatch />
+        </div>
       </div>
     );
   }
